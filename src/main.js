72c5b5f21b61d342641f2724c71b4413fdad796a
zst <2356001919@qq.com>
2024-07-11 09:01:51 +0800

    修改配置项

--- src/main.js
+++ src/main.js
@@ -8,18 +8,23 @@ import "element-plus/dist/index.css";
 import "element-plus/theme-chalk/dark/css-vars.css";
 import "@/src/assets/style.scss";
 
-export default function _createApp() {
-  const app = createApp(h(ElConfigProvider, { locale: zhCn, message: { max: 2 } }, () => h(RouterView)));
-  const pinia = _createPinia();
-  const router = _createRouter();
-  app.use(router);
-  app.use(pinia);
-  app.use(directive);
-  app.use(components);
-  router.isReady().then(() => {
+export default async function _createApp() {
+  try {
+    const app = createApp(h(ElConfigProvider, { locale: zhCn, message: { max: 2 } }, () => h(RouterView)));
+    const pinia = _createPinia();
+    const router = _createRouter();
+    app.use(router);
+    app.use(pinia);
+    app.use(directive);
+    app.use(components);
+    await router.isReady();
     app.mount("#app");
-  });
-  return { app, pinia, router };
+    console.log("Application mounted successfully.");
+    return { app, pinia, router };
+  } catch (error) {
+    console.error("Failed to create the application:", error);
+    throw error;
+  }
 }
 
 _createApp();
