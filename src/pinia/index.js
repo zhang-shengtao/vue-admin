6c6f09db7cc4bcaa5a440b00bcd20c1c4dea546e
zhang-shengtao <71996292+zhang-shengtao@users.noreply.github.com>
2024-08-21 10:02:32 +0000

    Deploying to build from @ zhang-shengtao/vue-admin@b0a4305f9955a91531a1d5a2e8b86d4195c8d01a 🚀

--- src/pinia/index.js
+++
@@ -1,9 +0,0 @@
-export { default as userPinia } from "./user";
-import { createPinia } from "pinia";
-
-let pinia;
-export default function _createPinia() {
-  if (pinia) return pinia;
-  pinia = createPinia();
-  return pinia;
-}
--- src/router/modules/index.js
+++
@@ -1,5 +0,0 @@
-import admin from "./admin";
-import test from "./test";
-import visual from "./visual";
-
-export default [...admin, ...test, ...visual];
